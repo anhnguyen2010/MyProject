b62f71fcb5758ae73f9da00ee4edc4a4661df7ad
dunganh <nguyenhuudunganh@gmail.com>
2018-03-15 10:14:09 +0700

    đổ dữ liệu sản phẩm

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 use App\Slide;
+use App\Product;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function getIndex(){
         $slide = Slide::all();
-        print_r(@$slide);
-
-        return view('page.trangchu', compact('slide'));
+        $new_product=Product::where('new', 1)->get();
+        return view('page.trangchu', compact('slide', 'new_product'));
     }
     public  function getLoaiSp(){
         return view('page.loai_sanpham');
