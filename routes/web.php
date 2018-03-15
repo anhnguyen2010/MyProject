786d0718cba0d78e1db9c3d99a99ec45d9813722
dunganh <nguyenhuudunganh@gmail.com>
2018-03-15 09:15:54 +0700

    cắt trang

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,23 @@ Route::get('them-san-pham', function () {
     $producttype = DB::table('product_type')->select('name')->get();;
     return view('Admin.Pages.insert', ['producttype' => $producttype]);
 });
+router::get('index', [
+    'as'=>'trang-chu',
+    'uses'=>'PageController@getIndex'
+]);
+router::get('loai-san-pham', [
+    'as'=>'loaisanpham',
+    'uses'=>'PageController@getLoaiSp'
+]);
+router::get('chi-tiet-san-pham', [
+    'as'=>'chitietsanpham',
+    'uses'=>'PageController@getChitiet'
+]);
+router::get('lien-he', [
+    'as'=>'lienhe',
+    'uses'=>'PageController@getLienHe'
+]);
+router::get('gioi-thieu', [
+    'as'=>'gioithieu',
+    'uses'=>'PageController@getGioiThieu'
+]);
